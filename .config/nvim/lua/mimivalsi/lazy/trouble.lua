af05085ff45c37abd1f3d4deb870d3e463c0168a
Miguel DA SILVA <jmiguel.dasilvaf@proton.me>
2024-03-08 21:59:43 +0100

    feat: add trouble

---
+++ .config/nvim/lua/mimivalsi/lazy/trouble.lua
@@ -0,0 +1,21 @@
+return {
+ "folke/trouble.nvim",
+ dependencies = { "nvim-tree/nvim-web-devicons" },
+ opts = {
+ },
+    config = function ()
+	-- local actions = require("telescope.actions")
+	local trouble = require("trouble.providers.telescope")
+
+	local telescope = require("telescope")
+
+	telescope.setup {
+	    defaults = {
+		mappings = {
+		    i = { ["<c-t>"] = trouble.open_with_trouble },
+		    n = { ["<c-t>"] = trouble.open_with_trouble },
+		},
+	    },
+	}
+    end
+}
