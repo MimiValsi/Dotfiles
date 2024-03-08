38e9004596127345c64be03ba0a02b98e44645ae
Miguel DA SILVA <jmiguel.dasilvaf@proton.me>
2024-03-08 22:02:40 +0100

    feat: add vimgo

--- .config/nvim/lua/mimivalsi/lazy/govim.lua
+++ .config/nvim/lua/mimivalsi/lazy/govim.lua
@@ -1,6 +1,6 @@
 return {
-  --  "fatih/vim-go",
+    "fatih/vim-go",
 
-  -- dirty way to change vim-go settings
-  --vim.cmd("let g:go_gopls_gofumpt=1"),
+    -- dirty way to change vim-go settings
+    vim.cmd("let g:go_gopls_gofumpt=1"),
 }
