51fdc0422166ceab1b7b429a11529ec0f21ff235
Miguel DA SILVA <jmiguel.dasilvaf@proton.me>
2024-03-08 22:04:39 +0100

    feat: add tab nav and trouble.nvim keymaps

--- .config/nvim/lua/mimivalsi/remaps.lua
+++ .config/nvim/lua/mimivalsi/remaps.lua
@@ -4,6 +4,26 @@ local map = vim.keymap.set
 g.mapleader = " "
 -- normal mode
 
+-- navigate tabs
+map("n", "<Tab>", "gt")
+map("n", "<S-Tab>", "gT")
+
+-- trouble
+map("n", "<leader>tt", function() require("trouble").toggle() end)
+map("n", "<leader>tw", function() require("trouble").toggle("workspace_diagnostics") end)
+map("n", "<leader>td", function() require("trouble").toggle("document_diagnostics") end)
+map("n", "<leader>tq", function() require("trouble").toggle("quickfix") end)
+map("n", "<leader>tl", function() require("trouble").toggle("loclist") end)
+map("n", "gR", function() require("trouble").toggle("lsp_references") end)
+
+-- GoAddTags / GoRemoveTags
+map("n", "<leader>oo", "<cmd>GoAddTags<CR>")
+map("n", "<leader>op", "<cmd>GoRemoveTags<CR>")
+
+-- take a screenshot
+map("n", "<leader>ts", "<cmd>Screenshot<CR>")
+
+-- navigate between () or {} or []
 map("n", "m", "%")
 
 map("n", "<leader>v", "<cmd>vsplit<CR>")
@@ -50,4 +70,5 @@ map("v", "<leader>Y", '"+Y') -- yank to system keyboard
 map("v", "<leader>d", '"_d') -- delete in void register
 
 -- x mode need to find out
-map("x", "<leader>p", '"_dP')
+-- map("x", "<leader>p", '"_dP')
+map("n", "<leader>p", "<C-S-v>")
